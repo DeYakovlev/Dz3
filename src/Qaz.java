a2d612e25298ffc7d0ccd56efbfa634593fbd7c7
DeYakovlev <99YaDe@gmail.com>
2022-04-16 17:37:23 +0300

    Initial commit

---
+++ src/Qaz.java
@@ -0,0 +1,24 @@
+public class Qaz {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 3;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        if (a%2 == 0)
+        System.out.println("четное");
+        else
+        System.out.println("нечетное");
+
+        if(b%2 == 0)
+            System.out.println("четное");
+        else
+            System.out.println("нечетное");
+
+        System.out.println("\u263A");
+    }
+
+}
